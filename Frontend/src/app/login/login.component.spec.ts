865991d671175ee167a0cab869526c4f960393bb
Amiel <78980453+RiosAmiel@users.noreply.github.com>
2022-10-07 17:17:14 +0800

    started on unit testing

--- Frontend/src/app/login/login.component.spec.ts
+++ Frontend/src/app/login/login.component.spec.ts
@@ -1,23 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DataService } from '../services/data.service';
 import { LoginComponent } from './login.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { HttpClient } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let ds: DataService;
+  let http: HttpClient;
+  let httpCont: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DataService],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
+    ds = TestBed.inject(DataService);
+    http = TestBed.inject(HttpClient);
+    httpCont = TestBed.inject(HttpTestingController);
 
+    
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    expect(ds).toBeDefined();
     expect(component).toBeTruthy();
+  });
+  it('login api', () => {
+    expect(ds.getPosts()).toBeDefined();
   });
 });
--- Frontend/src/app/services/data.service.ts
+++ Frontend/src/app/services/data.service.ts
@@ -17,18 +17,8 @@ export class DataService {
     getPosts(): Observable<Users[]> {
       return this.http.get(`${environment.apiURL}`) as Observable<Users[]>;
     }
-    getUser(id: number): Observable<Users[]>{
-      return this.http.get(`${environment.apiURL}/${id}`) as Observable<Users[]>;
-    }
     getCars(): Observable<Cars[]>{
       return this.http.get(`${environment.carsURL}`) as Observable<Cars[]>;
     }
 
-    isUserLoggedIn(): boolean { 
-      return this.isLoggedIn 
-    }
-  
-    setUserLoggedIn(): void {
-      this.isLoggedIn = true;
-    }
 }
