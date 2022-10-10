58e64df1a1da9f42b6e21244685b5c21c5f73a4d
Amiel <78980453+RiosAmiel@users.noreply.github.com>
2022-10-10 11:30:24 +0800

    data service testing (on-going)

--- Frontend/src/app/login/login.component.spec.ts
+++ Frontend/src/app/login/login.component.spec.ts
@@ -1,38 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DataService } from '../services/data.service';
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
-import { HttpClient } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
-  let ds: DataService;
-  let http: HttpClient;
-  let httpCont: HttpTestingController;
+  let httSpy: { get: jasmine.Spy };
 
   beforeEach(async () => {
+    httSpy = jasmine.createSpyObj('HttpClient', ['get']);
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [DataService],
+      imports: [FormsModule],
       declarations: [ LoginComponent ]
     })
-    .compileComponents();
-    ds = TestBed.inject(DataService);
-    http = TestBed.inject(HttpClient);
-    httpCont = TestBed.inject(HttpTestingController);
-
     
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(ds).toBeDefined();
     expect(component).toBeTruthy();
-  });
-  it('login api', () => {
-    expect(ds.getPosts()).toBeDefined();
   });
 });
--- Frontend/src/app/services/data.service.spec.ts
+++ Frontend/src/app/services/data.service.spec.ts
@@ -1,16 +1,38 @@
-import { TestBed } from '@angular/core/testing';
-
+import { fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { DataService } from './data.service';
+import { from } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { doesNotReject } from 'assert';
 
 describe('DataService', () => {
   let service: DataService;
+  let http: HttpClient;
+  let httpCont: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers : [DataService],
+    });
     service = TestBed.inject(DataService);
+    http = TestBed.inject(HttpClient);
+    httpCont = TestBed.inject(HttpTestingController);
+    
   });
 
   it('should be created', () => {
+    expect(service).toBeDefined();
     expect(service).toBeTruthy();
+  });
+  it('userApi' , () =>{ 
+    service.getPosts().subscribe( res => 
+      expect(res.length).toBeGreaterThan(0));
+
+  });
+
+  it('carsApi', () => {
+    service.getCars().subscribe(res => 
+      expect(res.length).toBeGreaterThan(0));
   });
 });
